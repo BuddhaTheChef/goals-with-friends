2606ecda1008bad881eb2846356a314106a40561
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-05-28 13:55:33 -1000

    implemented redux and added firebase database

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h3>Goals</h3>
+        <h3>Goals With Friends</h3>
         <AddGoal />
         <div>Goal List</div>
           <button
